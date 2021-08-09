80b9eff53606829f2ae9b9dfda9750c721336947
01000110 <01000110@users.noreply.github.com>
2021-08-09 13:27:44 +0200

    update sidebar

--- src/App.js
+++ src/App.js
@@ -1,54 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import {BookOpenIcon, ClipboardListIcon, HomeIcon, LinkIcon, } from '@heroicons/react/outline'
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 const menuItems = [
     {
         name: "home",
-        icon: React.createElement(HomeIcon, {className: 'Menu-Item'}),
-        link: "/",
-        active: true
+        icon: React.createElement(HomeIcon, {className: 'Menu-Item', alt: 'home'}),
+        link: "/"
     },
     {
         name: "loans and orders",
         icon: React.createElement(BookOpenIcon, {className: 'Menu-Item'}),
-        link: "/loans",
-        active: false
+        link: "/loans"
     },
     {
         name: "lists",
         icon: React.createElement(ClipboardListIcon, {className: 'Menu-Item'}),
-        link: "/lists",
-        active: false
+        link: "/lists"
     },
     {
         name: "digital content",
         icon: React.createElement(LinkIcon, {className: 'Menu-Item'}),
-        link: "/digital",
-        active: false
+        link: "/digital"
     },
 ];
 
 function Sidebar() {
   return (
       <nav className="fixed flex justify-center left-0 bg-green-900 w-20 h-screen py-5 pl-2">
-          <div>
-              <ul className="text-white text-center">
-                  {
-                      menuItems.map( (item, index) =>
-                          <li data-active={item.active} key={index} className="items-center justify-center py-4">
-                              {item.icon}
-                          </li>
-                      )
-                  }
-                  <li className="flex justify-center pt-7">
-                      <h1 className="text-white text-center vertical-rl transform rotate-180">
-                          <span className="font-bold">Bergen</span><span className="font-thin">Bibliotek</span>
-                      </h1>
-                  </li>
-              </ul>
-          </div>
+          <ul className="text-white text-center">
+              {
+                  menuItems.map( (item, index) =>
+                      <li key={index} className="items-center justify-center py-4">
+                          <NavLink end to={item.link} activeClassName="active">{item.icon}</NavLink>
+                      </li>
+                  )
+              }
+              <li key="logo" className="flex justify-center pt-7">
+                  <h1 className="text-white text-center vertical-rl transform rotate-180">
+                      <span className="font-bold">Bergen</span><span className="font-thin">Bibliotek</span>
+                  </h1>
+              </li>
+          </ul>
       </nav>
   )
 }
